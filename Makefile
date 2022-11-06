676d0e8c126eeb6e8939aa681c67ba7cbbd4f09f
Dmitry Gubin <dmitrygubin@Dmitrys-Air.Dlink>
2022-11-06 12:47:14 +0300

    Second step complite

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:  #  create environment variables and dependencies
+		poetry install
+
+gendiff:  # run application
+			poetry run gendiff
+
+build:
+			poetry build
+
+publish:		
+			poetry publish --dry-run
+
+package-install:
+			python3 -m pip install --user dist/*.whl
+
+lint:
+			poetry run flake8 gendiff
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.',\
-                                     prog = 'gendiff')
+                                     prog = 'gendiff', epilog = "set format of output")
+    parser.add_argument('-f','--format')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.print_help()

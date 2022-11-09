634171f35f804ffafca133ec8886987c52f89247
Dmitry Gubin <dmitrygubin@Dmitrys-Air.Dlink>
2022-11-09 12:39:00 +0300

    Add badge codeclimat

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dgubin71/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/dgubin71/python-project-50/actions)
+
+<a href="https://codeclimate.com/github/dgubin71/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/2371bace3875e27bedf8/maintainability" /></a>
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,5 +1,6 @@
 import json
 
+
 def bool_to_text(var):
     if isinstance(var, bool):
         if var:
@@ -8,12 +9,12 @@ def bool_to_text(var):
             return 'false'
     return var
 
+
 def generate_diff(path_file1, path_file2):
     dict_1 = json.load(open(path_file1))
     dict_2 = json.load(open(path_file2))
 
-    dict_to_str = '{'+'\n'
-    dict2_to_str = ''
+    dict_to_str = '{' + '\n'
     key1_lst = []
     key2_lst = []
 
@@ -26,16 +27,14 @@ def generate_diff(path_file1, path_file2):
     for key in key1_lst:
         if dict_2.get(key, '-') != '-':
             if dict_2.get(key) == dict_1.get(key):
-                dict_to_str = dict_to_str +'   '+ key + ': ' + str(bool_to_text(dict_1.get(key))) + '\n'
+                dict_to_str = dict_to_str + '   ' + key + ': ' + str(bool_to_text(dict_1.get(key))) + '\n'
             else:
                 if dict_1.get(key, 'none') != 'none':
-                    dict_to_str = dict_to_str + ' - ' + key + ': ' + str(
-                        bool_to_text(dict_1.get(key))) + '\n' + ' + ' + key + ': ' + str(
-                        bool_to_text(dict_2.get(key))) + '\n'
+                    dict_to_str = dict_to_str + ' - ' + key + ': ' + str(bool_to_text(dict_1.get(key))) + '\n' + ' + ' + key + ': ' + str(bool_to_text(dict_2.get(key))) + '\n'
                 else:
                     dict_to_str = dict_to_str + ' + ' + key + ': ' + str(bool_to_text(dict_2.get(key))) + '\n'
         else:
             dict_to_str = dict_to_str + ' - ' + key + ': ' + str(bool_to_text(dict_1.get(key))) + '\n'
 
-    dict_to_str = dict_to_str +'}'
+    dict_to_str = dict_to_str + '}'
     return dict_to_str

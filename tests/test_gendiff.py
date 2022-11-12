c4f14b05097d6554f126c62db5444e3b393853fa
Dmitry Gubin <dmitrygubin@Dmitrys-Air.Dlink>
2022-11-13 01:18:45 +0300

    5 step fixtures and tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -20,7 +20,7 @@ def test_generate_json_no_diff():
 def test_generate_yaml_diff():
     path_file1 = './tests/fixtures/file1.yaml'
     path_file2 = './tests/fixtures/file2.yaml'
-    diff = open('./tests/fixtures/output_diff_yaml.txt')
+    diff = open('./tests/fixtures/output_diff_yaml')
     result = diff.read()
     assert generate_diff(path_file1, path_file2) == result
 
@@ -28,7 +28,7 @@ def test_generate_yaml_diff():
 def test_generate_yaml_no_diff():
     path_file1 = './tests/fixtures/file1.yaml'
     path_file2 = './tests/fixtures/file1.yaml'
-    diff = open('./tests/fixtures/output_diff2_yaml.txt')
+    diff = open('./tests/fixtures/output_diff2_yaml')
     result = diff.read()
     assert generate_diff(path_file1, path_file2) == result
 
@@ -36,7 +36,7 @@ def test_generate_yaml_no_diff():
 def test_generate_yml_diff():
     path_file1 = './tests/fixtures/file1.yml'
     path_file2 = './tests/fixtures/file2.yml'
-    diff = open('./tests/fixtures/output_diff_yml.txt')
+    diff = open('./tests/fixtures/output_diff_yml')
     result = diff.read()
     assert generate_diff(path_file1, path_file2) == result
 
@@ -44,6 +44,7 @@ def test_generate_yml_diff():
 def test_generate_yml_no_diff():
     path_file1 = './tests/fixtures/file1.yml'
     path_file2 = './tests/fixtures/file1.yml'
-    diff = open('./tests/fixtures/output_diff2_yml.txt')
+    diff = open('./tests/fixtures/output_diff2_yml')
     result = diff.read()
     assert generate_diff(path_file1, path_file2) == result
+    

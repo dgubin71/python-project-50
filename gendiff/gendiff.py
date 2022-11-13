3058ceb966cfbe769abe0b378ad9d7d947740ca6
Dmitry Gubin <dmitrygubin@Dmitrys-Air.Dlink>
2022-11-13 16:42:28 +0300

    5 step fix gendiff.py, test_gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,8 +3,8 @@ from gendiff.parsers import diff_yaml
 
 
 def generate_diff(path_file1, path_file2):
-    if path_file1.find('.json', -5 != -1) and \
-            path_file2.find('.json', -5 != -1):
+    if path_file1.find('.json', -5) != -1 and \
+            path_file2.find('.json', -5) != -1:
         return (diff_json.generate_diff_json(path_file1, path_file2))
     if path_file1.find('.yaml', -5) != -1 and\
             path_file2.find('.yaml', -5) != -1:
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,47 +4,46 @@ from gendiff.gendiff import generate_diff
 def test_generate_json_diff():
     path_file1 = './tests/fixtures/file1.json'
     path_file2 = './tests/fixtures/file2.json'
-    diff = open('./tests/fixtures/output_diff')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
 
 
 def test_generate_json_no_diff():
     path_file1 = './tests/fixtures/file1.json'
     path_file2 = './tests/fixtures/file1.json'
-    diff = open('./tests/fixtures/output_diff2')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff2')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
 
 
 def test_generate_yaml_diff():
     path_file1 = './tests/fixtures/file1.yaml'
     path_file2 = './tests/fixtures/file2.yaml'
-    diff = open('./tests/fixtures/output_diff_yaml')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff_yaml')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
 
 
 def test_generate_yaml_no_diff():
     path_file1 = './tests/fixtures/file1.yaml'
     path_file2 = './tests/fixtures/file1.yaml'
-    diff = open('./tests/fixtures/output_diff2_yaml')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff2_yaml')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
 
 
 def test_generate_yml_diff():
     path_file1 = './tests/fixtures/file1.yml'
     path_file2 = './tests/fixtures/file2.yml'
-    diff = open('./tests/fixtures/output_diff_yml')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff_yml')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
 
 
 def test_generate_yml_no_diff():
     path_file1 = './tests/fixtures/file1.yml'
     path_file2 = './tests/fixtures/file1.yml'
-    diff = open('./tests/fixtures/output_diff2_yml')
-    result = diff.read()
+    diff_file = open('./tests/fixtures/output_diff2_yml')
+    result = diff_file.read()
     assert generate_diff(path_file1, path_file2) == result
-    
